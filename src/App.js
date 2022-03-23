7a7f8ae8b6bbdfaf32f0fe36499f9d1dd7ce63c2
Irwanto Sirait <#irwanto.saputra.sirait@gmail.com>
2022-03-23 16:06:26 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+                 <Route path="/" component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+              <Route render={() => <h1>404: page not found</h1>} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  } 
+}
+
+
+
+export default App;
